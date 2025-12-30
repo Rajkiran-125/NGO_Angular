9ee9cf429c3ea2442942168bd3f7c447ee12a780
Shreyansh2610 <shreyansh7874056628@gmail.com>
2025-12-30 23:25:04 +0530

    docker env url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,11 @@
 export const environment = {
   production: false,
 
-  apiUrl: 'http://localhost:3000/api/',
-  fileBaseUrl: 'http://localhost:3000'
+  // apiUrl: 'http://localhost:3000/api/',
+  // fileBaseUrl: 'http://localhost:3000'
+
+  apiUrl: 'http://72.61.245.218:3000/api/',
+  fileBaseUrl: 'http://72.61.245.218:3000'
 
   // apiUrl: 'https://vmsbackend-eudv.onrender.com/api/',
   // fileBaseUrl: 'https://vmsbackend-eudv.onrender.com/uploads/'
