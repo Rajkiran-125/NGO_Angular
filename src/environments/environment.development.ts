693de561d967c6b5ce12290df68ed9249241bc74
Shreyansh2610 <shreyansh7874056628@gmail.com>
2026-01-02 22:27:25 +0530

    env updated

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,12 +1,15 @@
 export const environment = {
   production: false,
+
   // apiUrl: 'http://localhost:3000/api/',
   // fileBaseUrl: 'http://localhost:3000'
 
-  //   apiUrl: 'https://vmsbackend-eudv.onrender.com/api/',
+  apiUrl: 'https://volunteer.nest4us.org/api/',
+  fileBaseUrl: 'https://volunteer.nest4us.org'
+
+  // apiUrl: 'https://vmsbackend-eudv.onrender.com/api/',
   // fileBaseUrl: 'https://vmsbackend-eudv.onrender.com/uploads/'
 
-
-  apiUrl: 'https://ngo-node-js.onrender.com/api/',
-  fileBaseUrl: 'https://ngo-node-js.onrender.com'
+  // apiUrl: 'https://ngo-node-js.onrender.com/api/',
+  // fileBaseUrl: 'https://ngo-node-js.onrender.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
-  production: false,
+  production: true,
 
-  // apiUrl: 'http://localhost:3000/api/',
-  // fileBaseUrl: 'http://localhost:3000'
+  apiUrl: 'https://volunteer.nest4us.org/api/',
+  fileBaseUrl: 'https://volunteer.nest4us.org'
 
-  apiUrl: 'http://72.61.245.218:3000/api/',
-  fileBaseUrl: 'http://72.61.245.218:3000'
+  // apiUrl: 'http://72.61.245.218:3000/api/',
+  // fileBaseUrl: 'http://72.61.245.218:3000'
 
   // apiUrl: 'https://vmsbackend-eudv.onrender.com/api/',
   // fileBaseUrl: 'https://vmsbackend-eudv.onrender.com/uploads/'
